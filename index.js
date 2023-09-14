fa16762702d97dda6e48f5eaa218b9f17185432c
Emmanue <kioxmanu7@gmail.com>
2023-09-14 12:39:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+console.log(cats);
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  const catsCopy = [...cats, name];
+  return catsCopy;
+}
+
+function prependCat(name) {
+  const catsCopy = [name, ...cats];
+  return catsCopy;
+}
+function removeLastCat() {
+  const newArray = [...cats];
+  newArray.pop();
+  return newArray;
+}
+
+
+function removeFirstCat() {
+  const updatedCats = cats.slice(1); 
+  return updatedCats;
+}
